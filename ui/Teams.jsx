928f30aaacea755ac74a348e0818970d0296f9d4
SeemaKumari <seemakumari62998@gmail.com>
2024-02-22 17:18:18 +0530

    menu list

--- ui/Teams.jsx
+++ ui/Teams.jsx
@@ -3,17 +3,17 @@ import styled from "styled-components";
 const Container = styled.div`
   height: 100vh;
   display: flex;
-  align-items: center;
-  justify-content: center;
+
   color: var(--color-grey-200);
   gap: 40px;
 `;
 
 const Section = styled.div`
   border: 1px solid white;
-  width: 25%;
+  width: 30%;
   padding: 10px;
   text-align: left;
+  margin-top: 40px;
 `;
 
 const Image = styled.img`
@@ -23,31 +23,40 @@ const Image = styled.img`
   max-height: 100%;
 `;
 
+const H1 = styled.h1`
+  color: white;
+  text-align: center;
+  margin-top: 100px;
+`;
+
 function Teams() {
   return (
-    <Container>
-      <Image src="" alt="photo" />
-      <Section>
-        <h3>
-          Ratul Borah <br /> Founder and CEO
-        </h3>
-        <p>
-          B.E., M. Engg. Entrepreneur and innovation expert 26 years of
-          industrial & business experience 20 years of motor design, development
-          & manufacturing experience
-        </p>
-      </Section>
+    <>
+      <H1>Elecnovo Teams</H1>
+      <Container>
+        <Image src="" alt="photo" />
+        <Section>
+          <h3>
+            Ratul Borah <br /> Founder and CEO
+          </h3>
+          <p>
+            B.E., M. Engg. Entrepreneur and innovation expert 26 years of
+            industrial & business experience 20 years of motor design,
+            development & manufacturing experience
+          </p>
+        </Section>
 
-      <Image src="" alt="photo" />
-      <Section>
-        <p>Santanoo Medhi Co-Founder</p>
-        <p>
-          B.E., M. Engg. 9 years experience as CEO / MD 28 years of management &
-          manufacturing 17 years of motor design, development & manufacturing
-          experience
-        </p>
-      </Section>
-    </Container>
+        <Image src="" alt="photo" />
+        <Section>
+          <p>Santanoo Medhi Co-Founder</p>
+          <p>
+            B.E., M. Engg. 9 years experience as CEO / MD 28 years of management
+            & manufacturing 17 years of motor design, development &
+            manufacturing experience
+          </p>
+        </Section>
+      </Container>
+    </>
   );
 }
 
